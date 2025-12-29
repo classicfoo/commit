de2624611151c4ce7fb491c670743dea2c53b4b2
classicfoo <michaelhuynh4000@gmail.com>
2025-12-29 19:06:57 +1100

    Update logged-out header and notifications

--- auth_footer.php
+++ auth_footer.php
@@ -1,4 +1,11 @@
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+    <script>
+      window.setTimeout(() => {
+        document.querySelectorAll('[data-auto-dismiss="true"]').forEach((alert) => {
+          alert.style.display = 'none';
+        });
+      }, 4000);
+    </script>
   </body>
 </html>
